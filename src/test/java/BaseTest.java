45789623801804b5608daf088478f2e2a5dd7b12
Denis Platonov <denis-platonov@users.noreply.github.com>
2022-11-05 18:19:34 -0700

    Adds support of CLI properties

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,11 +2,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 public class BaseTest {
 
@@ -15,83 +23,101 @@ public class BaseTest {
     WebDriverWait wait;
     Actions actions;
 
+    static String where = System.getProperty("where");
+    static String browser = System.getProperty("browser");
+    static String env = System.getProperty("env");
+    static String suite = System.getProperty("suite");
+
+    static String parameters = where + " " + browser + " " + env + " " + suite;
 
     @BeforeSuite
-    public static void chromeConfigs() {
+    public static void chromeConfig() {
         // This is for Windows users
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         } else {
             System.setProperty("webdriver.chrome.driver", "Untitled/chromedriver");
         }
+        // Just checking
+        System.out.println(parameters);
+    }
+
+    private WebDriver getDriver() throws MalformedURLException {
+        if(where.contains("selenoid")) {
+            return configRemoteWebDriver();
+        }
+        else if (where.contains("lambdatest")){
+            return configLambdaTestDriver();
+        }
+        if(browser.contains("safari")){
+            return new SafariDriver();
+        }
+        else if(browser.contains("firefox")){
+            return new FirefoxDriver();
+        }
+        else return new ChromeDriver();
+    }
+
+    private WebDriver configLambdaTestDriver() throws MalformedURLException {
+        String username = System.getenv("LT_USERNAME") == null ? "dancefront" : System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Q3kapiUPNjt4PRRUTA4Hx6SEK523Lpi5vGF148aniE6qcdxcwx" : System.getenv("LT_ACCESS_KEY");
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        // Configure your capabilities here
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("version", "92.0");
+        caps.setCapability("resolution", "1024x768");
+        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("name", this.getClass().getName());
+        caps.setCapability("plugin", "git-testng");
+
+        String[] Tags = new String[] { "Feature", "Magicleap", "Severe" };
+        caps.setCapability("tags", Tags);
+
+        return new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
+    }
+
+    private WebDriver configRemoteWebDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "opera");
+        capabilities.setCapability("browserVersion", "89.0");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", false,
+                "enableVideo", false
+        ));
+        RemoteWebDriver driver = new RemoteWebDriver(
+                URI.create("http://104.237.11.187:4444/wd/hub").toURL(),
+                capabilities);
+        return driver;
+    }
+
+    private String getUrl(){
+        if (env != null) {
+            if (env.contains("dev")) {
+                return "https://dev.koel.app";
+            } else if (env.contains("stage")) {
+                return "https://stage.koel.app";
+            } else if (env.contains("prod")) {
+                return "https://koel.app";
+            }
+        }
+        return "https://bbb.testpro.io";
     }
 
     @BeforeMethod
-    // Send a parameter for 'baseURL' specified in XML
-    @Parameters({"baseURL"})
-    // Make baseURL parameter optional, if it is null, then set it to something)
-    public void launchBrowser(@Optional String baseURL) {
-        if (baseURL == null)
-            baseURL ="https://bbb.testpro.io";
-        driver = new ChromeDriver();
+    public void launchBrowser() throws MalformedURLException {
+        url = getUrl();
+        driver = getDriver();
         actions = new Actions(driver);
-        // Make webdriver load the pages REALLY slow
-//        WebDriver augmentedDriver = new Augmenter().augment(driver);
-//        ChromiumNetworkConditions networkConditions = new ChromiumNetworkConditions();
-//        networkConditions.setDownloadThroughput(100 * 1024);
-//        networkConditions.setUploadThroughput(500 * 1024);
-//        networkConditions.setLatency(Duration.ofMillis(5000));
-//        ((HasNetworkConditions) augmentedDriver).setNetworkConditions(networkConditions);
-        // (comment out above lines to remove throttling)
-
-        // Wait for an element to show up for max of X seconds
-        // implicitlyWait(Duration.ofSeconds(60) will wait for UP to 60 seconds
-        // if element comes up after 1 second, it will move on
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
         wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-        // thread.sleep(60000) -- will wait 60s always
-        url = baseURL;
         driver.get(url);
-
     }
 
     @AfterMethod
     public void tearDownBrowser() {
         driver.quit();
-    }
-
-    public void clickSubmitBtn() {
-        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
-        submitButton.click();
-    }
-
-    public void provideEmail(String email) {
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        emailField.click();
-        emailField.sendKeys(email);
-    }
-
-    public void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-        passwordField.click();
-        passwordField.sendKeys(password);
-
-    }
-
-    @DataProvider(name="invalidCredentials")
-    public static Object[][] getCredentials(){
-
-        return new Object[][] {
-                {"invalid@class.com", "invalidPass"},
-                {"d@class.com", ""},
-                {"", ""}
-        };
-    }
-
-    public void login(){
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmitBtn();
     }
 }
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,52 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(css="[type='email']")
+    WebElement emailField;
+
+    @FindBy(css="[type='password']")
+    WebElement passwordField;
+
+    @FindBy(css="[type='submit']")
+    WebElement submitButton;
+
+    @FindBy(css="[data-testid='login-form']")
+    WebElement loginForm;
+
+    public LoginPage(WebDriver givenDriver){
+        super(givenDriver);
+    }
+
+    public HomePage submit() throws InterruptedException {
+        submitButton.click();
+        Thread.sleep(1000); // to generate the video
+        return new HomePage(driver);
+    }
+
+    public LoginPage enterEmail(String email) throws InterruptedException {
+        emailField.sendKeys(email);
+        Thread.sleep(1000); // to generate the video
+        return this;
+    }
+
+    public LoginPage enterPassword(String password) throws InterruptedException {
+        passwordField.sendKeys(password);
+        Thread.sleep(1000); // to generate the video
+        return this;
+    }
+
+    public HomePage login() throws InterruptedException {
+        enterEmail("demo@class.com");
+        enterPassword("te$t$tudent");
+        return submit();
+    }
+
+    public boolean isErrorShown() {
+        return loginForm.getAttribute("class").equals("error");
+    }
+}
