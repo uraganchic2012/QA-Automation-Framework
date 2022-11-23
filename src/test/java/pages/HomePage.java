dcc4c54bee698cb689e8d9aa48ac44044a10b6f7
Denis Platonov <denis-platonov@users.noreply.github.com>
2022-11-22 20:33:55 -0800

    adds create a playlist test

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,10 +1,46 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class HomePage extends BasePage{
 
+    @FindBy(css = "[data-testid='sidebar-create-playlist-btn']")
+    WebElement createPlaylistButton;
+
+    @FindBy(css = "[data-testid='playlist-context-menu-create-simple']")
+    WebElement newPlaylistOption;
+
+    @FindBy(css = "[name='name']")
+    WebElement playlistNameField;
+
+    @FindBys(
+            @FindBy(css = "#playlists a[href*='playlist']")
+    )
+    List<WebElement> playlists;
+
     public HomePage(WebDriver sentDriver) {
         super(sentDriver);
+    }
+
+    public void createPlaylist(String name) throws InterruptedException {
+        createPlaylistButton.click();
+        newPlaylistOption.click();
+        playlistNameField.sendKeys(name);
+        playlistNameField.sendKeys(Keys.RETURN);
+        new WebDriverWait(driver, Duration.ofSeconds(1)).until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("[name='name']")));
+    }
+
+    public int getNumberOfPlaylist() {
+        return playlists.size();
     }
 }
